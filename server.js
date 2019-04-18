8a89e0c135f1dc898421fa40073f69a37d9081ca
Facundo Rossi <rossifacundo94@gmail.com>
2019-04-18 16:21:07 -0300

    Improve Structure

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
 var express = require('express'),
   app = express(),
   port = process.env.PORT || 5000, 
-  //database = require('./api/config/database'),
   bodyParser = require('body-parser'),
   mongoose = require('mongoose');
-  boardRoutes = require('./routes/BoardRoutes'),
-  //listRoutes = require('./api/routes/ListRoutes'),
-  //cardRoutes = require('./api/routes/CardRoutes'),
-  //boardDAO = require('./api/dao/BoardDAO');
+  boardRoutes = require('./api/routes/BoardRoutes'),
 
-//boardDAO.populate()
 mongoose.connect('mongodb://localhost:27017/myboard', { useNewUrlParser: true })
 
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -20,9 +15,7 @@ app.use(function(req, res, next) {
   next()
 })
 
-//listRoutes(app)
 boardRoutes(app)
-//cardRoutes(app)
 
 app.listen(port, () => {
   console.log('Express server listening on port ' + port)
